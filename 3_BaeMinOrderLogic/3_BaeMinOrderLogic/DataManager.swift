d80cc3450c4dd7dd59bd53c16386f030a21fe1ee
dami <dami8789@gmail.com>
2023-10-03 19:53:36 +0900

    데이터 추가

--- 3_BaeMinOrderLogic/3_BaeMinOrderLogic/DataManager.swift
+++ 3_BaeMinOrderLogic/3_BaeMinOrderLogic/DataManager.swift
@@ -80,25 +80,40 @@ class MenuCheckBoxDataManager{
     private var menuCheckBoxDataArray: [MenuCheckBoxSection] = []
     func makeMenuCheckBoxData() {
         menuCheckBoxDataArray = [
+            //도우 추가선택
             MenuCheckBoxSection(menu: [
                 MenuCheckBox(checkBoxSelected: false, menu: "치즈크러스트", price: 3000, sectionNum : 2),
-                MenuCheckBox(checkBoxSelected: false, menu: "고구마크러스트", price: 5000,sectionNum : 2),
+                MenuCheckBox(checkBoxSelected: false, menu: "고구마크러스트", price: 3000,sectionNum : 2),
                 MenuCheckBox(checkBoxSelected: false, menu: "골드", price: 6000, sectionNum : 2),
-                MenuCheckBox(checkBoxSelected: false, menu: "소보로", price: 2000, sectionNum : 2)]),
-            
+                MenuCheckBox(checkBoxSelected: false, menu: "소보로", price: 5000, sectionNum : 2)]),
+            //피자 추가선택
             MenuCheckBoxSection(menu: [
               
-                MenuCheckBox(checkBoxSelected: false, menu: "치즈 추가", price: 3600 , sectionNum : 3)]),
-            
+                MenuCheckBox(checkBoxSelected: false, menu: "치즈 추가", price: 2500 , sectionNum : 3)]),
+            //사이드 추가선택
             MenuCheckBoxSection(menu: [
-                MenuCheckBox(checkBoxSelected: false, menu: "치즈오븐스파게티", price: 2000, sectionNum : 4),
-                MenuCheckBox(checkBoxSelected: false, menu: "치킨텐더 9조각", price: 6000, sectionNum : 4),
-                MenuCheckBox(checkBoxSelected: false, menu: "치킨텐더 18조각", price: 5700, sectionNum : 4),
-                MenuCheckBox(checkBoxSelected: false, menu: "버팔로핫윙10조각", price: 2300, sectionNum : 4)]),
-                
+                MenuCheckBox(checkBoxSelected: false, menu: "치즈오븐스파게티", price: 8000, sectionNum : 4),
+                MenuCheckBox(checkBoxSelected: false, menu: "치킨텐더 9조각", price: 9900, sectionNum : 4),
+                MenuCheckBox(checkBoxSelected: false, menu: "치킨텐더 18조각", price: 19000, sectionNum : 4),
+                MenuCheckBox(checkBoxSelected: false, menu: "버팔로핫윙10조각", price: 9900, sectionNum : 4),
+                MenuCheckBox(checkBoxSelected: false, menu: "버팔로핫윙20조각", price: 19000, sectionNum : 4),
+                MenuCheckBox(checkBoxSelected: false, menu: "닭다리순살한입치킨(300g)", price: 7900, sectionNum : 4),
+                MenuCheckBox(checkBoxSelected: false, menu: "훈제치킨1마리", price: 13000, sectionNum : 4),
+                MenuCheckBox(checkBoxSelected: false, menu: "해쉬브라운5개", price: 5500, sectionNum : 4),
+                MenuCheckBox(checkBoxSelected: false, menu: "치즈스틱 6개", price: 5500, sectionNum : 4),
+                MenuCheckBox(checkBoxSelected: false, menu: "새우링 6개", price: 4000, sectionNum : 4),
+                MenuCheckBox(checkBoxSelected: false, menu: "새우링 10개", price: 7500, sectionNum : 4)]),
+                //기타추가선택
             MenuCheckBoxSection(menu: [
-                MenuCheckBox(checkBoxSelected: false, menu: "치즈크러스트", price: 35800, sectionNum : 5),
-                MenuCheckBox(checkBoxSelected: false, menu: "치즈크러스트", price: 34320, sectionNum : 5)])
+                MenuCheckBox(checkBoxSelected: false, menu: "콜라 1.25L", price: 3000, sectionNum : 5),
+                MenuCheckBox(checkBoxSelected: false, menu: "콜라 500ml", price: 2000, sectionNum : 5),
+                MenuCheckBox(checkBoxSelected: false, menu: "스프라이트 1.5L", price: 3000, sectionNum : 5),
+                MenuCheckBox(checkBoxSelected: false, menu: "스프라이트 500ml", price: 2000, sectionNum : 5),
+                MenuCheckBox(checkBoxSelected: false, menu: "핫불소스", price: 500, sectionNum : 5),
+                MenuCheckBox(checkBoxSelected: false, menu: "할라피뇨", price: 500, sectionNum : 5),
+                MenuCheckBox(checkBoxSelected: false, menu: "후레쉬피클", price: 500, sectionNum : 5),
+                MenuCheckBox(checkBoxSelected: false, menu: "파마산치즈가루", price: 200, sectionNum : 5),
+                MenuCheckBox(checkBoxSelected: false, menu: "핫소스", price: 200, sectionNum : 5)])
             
             
         ]
